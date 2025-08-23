2e8e61839c7f89e0da6c2717847c7300f4f30c3f
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-24 02:37:44 +0300

    feat(avatar): create UserAvatar component with props

---
+++ src/components/shared/UserAvatar/UserAvatar.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="avatar">
+    <img :src="src" :alt="alt" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+interface AvatarProps extends HTMLAttributes {
+  src: string;
+  alt?: string;
+  size?: number;
+}
+
+const {
+    src, alt
+} = defineProps<AvatarProps>();
+</script>
+
+<style scoped>
+  
+</style>
