cfd9e063588d1003653d60ab677d657a5e991d75
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-24 02:37:05 +0300

    feat(caption): create basic ItemCaption component with props

---
+++ src/components/shared/ItemCaption/ItemCaption.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <template
+        v-for="(item, itemIdx) in items"
+        :key="item.name"
+    >
+        <span>
+            <span>
+                {{ item.icon }}
+            </span>
+            <span>
+                {{ item.name }}
+            </span>
+        </span>
+        <span v-if="itemIdx < items.length - 1">
+            •
+        </span>
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+  interface CaptionProps extends HTMLAttributes {
+    items: {
+        name: string,
+        icon: string // TODO: icon props
+    }[],
+  }
+  
+  const {
+    items,
+  } = defineProps<CaptionProps>();
+</script>
+
+<style scoped>
+  
+</style>
