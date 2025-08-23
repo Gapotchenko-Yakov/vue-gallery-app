2be890bd2fba8cd308e33552ee920c432e1a515a
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-24 02:37:26 +0300

    feat(tag): create ItemTag component with props

---
+++ src/components/shared/ItemTag/ItemTag.vue
@@ -0,0 +1,29 @@
+<template>
+    <span>
+    <span>
+    {{ label }}
+  </span>
+  <button v-if="interactive">
+    {{ checked ? '➕' : '✔' }}
+  </button>
+    </span>
+</template>
+
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue';
+
+    interface ItemTagProps extends HTMLAttributes {
+        label: string,
+        variant?: '',
+        checked: boolean,
+        interactive: boolean,
+    }
+
+    const {
+        label, checked, interactive
+    } = defineProps<ItemTagProps>();
+</script>
+
+<style scoped>
+  
+</style>
