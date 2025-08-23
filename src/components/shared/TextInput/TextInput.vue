756b964b5459fadaaa8e5e6a7c2cccb80d335cba
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-24 02:37:35 +0300

    feat(input): create TextInput component with props

---
+++ src/components/shared/TextInput/TextInput.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="input-wrapper">
+        <slot name="left-icon" />
+        <input 
+            :value="modelValue" 
+            :placeholder="placeholder" 
+            :disabled="disabled" 
+            @input="handleChange"
+        />
+        <app-button
+            v-if="showClear && modelValue"
+            type="button"
+            variant="clear"
+        >
+            ✕
+        </app-button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { InputHTMLAttributes } from 'vue';
+
+interface InputProps  extends InputHTMLAttributes {
+  modelValue: string;
+  placeholder?: string;
+  disabled?: boolean;
+  showClear?: boolean;
+}
+
+const {
+    modelValue,
+    placeholder
+} = defineProps<InputProps>();
+
+const handleChange = () => {
+    console.log("🚀 ~ handleChange ~ handleChange:", handleChange)
+}
+</script>
+
+<style scoped>
+  
+</style>
