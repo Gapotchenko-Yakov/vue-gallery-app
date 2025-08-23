507e479a79add1182e036652729080ab05942c19
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-24 02:32:32 +0300

    feat(button): create basic AppButton component with props

---
+++ src/components/shared/AppButton/AppButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    :disabled="disabled"
+    @click="handleClick"
+    :type="type"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+import { 
+    defineProps, 
+    type ButtonHTMLAttributes 
+} from 'vue';
+
+interface ButtonProps extends ButtonHTMLAttributes {
+  text: string;                
+  variant: 'primary' | 'secondary';
+  disabled?: boolean; 
+}
+
+const {
+    text,
+    disabled,
+} = defineProps<ButtonProps>();
+
+const handleClick = (e: MouseEvent) => {
+    console.log("🚀 ~ handleClick ~ e.target:", e.target)
+}
+</script>
+
+<style scoped>
+
+</style>
