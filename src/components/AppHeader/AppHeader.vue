4b31ab099d0637d3587c5f639223a28d038be770
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-25 00:03:42 +0300

    feat(header): create AppHeader component with props

---
+++ src/components/AppHeader/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <app-icon
+        name="menu"
+    />
+    <app-icon
+        name="logo"
+    />
+    <nav-bar v-if="showNavLinks"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+    interface AppHeaderProps {
+        showNavLinks?: boolean;
+    }
+
+    const {
+        showNavLinks
+    } = defineProps<AppHeaderProps>();
+</script>
+
+<style scoped>
+  
+</style>
