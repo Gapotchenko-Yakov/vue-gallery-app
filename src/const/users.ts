2f37d7a996ee681c3406730041e6fd50dcec898e
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-24 20:29:40 +0300

    chore(user): add users constants

---
+++ src/const/users.ts
@@ -0,0 +1,8 @@
+import type { UserData } from "@/types/user";
+
+export const users: UserData[] = [
+    { id: 'u1', firstName: 'Иван', lastName: 'Иванов', birthDate: '1990-05-12', avatar: 'https://i.pravatar.cc/40?img=1' },
+    { id: 'u2', firstName: 'Мария', lastName: 'Петрова', birthDate: '1992-11-23', avatar: 'https://i.pravatar.cc/40?img=2' },
+    { id: 'u3', firstName: 'Алексей', lastName: 'Смирнов', birthDate: '1988-03-10', avatar: 'https://i.pravatar.cc/40?img=3' },
+    { id: 'u4', firstName: 'Елена', lastName: 'Кузнецова', birthDate: '1995-07-05', avatar: 'https://i.pravatar.cc/40?img=4' },
+];
