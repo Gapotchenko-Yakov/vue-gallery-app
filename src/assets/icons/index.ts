c728bcc4d1e4118ba0f06049b1ca94ba83b46e70
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-25 21:00:46 +0300

    chore: add plus and check icons

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -7,6 +7,8 @@ import Messages from './messages.svg';
 import SearchList from './search-list.svg';
 import Search from './search.svg';
 import Time from './time.svg';
+import Plus from './plus.svg';
+import Check from './check.svg';
 
 export const icons = {
     'chevron-down': ChevronDown,
@@ -18,6 +20,8 @@ export const icons = {
     'search-list': SearchList,
     'search': Search,
     'time': Time,
+    'plus': Plus,
+    'check': Check,
 } as const;
 
 export type IconName = keyof typeof icons;
