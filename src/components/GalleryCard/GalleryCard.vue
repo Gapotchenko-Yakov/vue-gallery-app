07d820c1f70ce01ed75be1021e5f2b0a9751029a
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-25 22:36:45 +0300

    feat(card): format date and add plural comments in metaCaptions

--- src/components/GalleryCard/GalleryCard.vue
+++ src/components/GalleryCard/GalleryCard.vue
@@ -28,6 +28,7 @@
 </template>
 
 <script setup lang="ts">
+import { monthNames } from '@/const/date';
 import { mapMetaCodeToIcon } from '@/const/posts';
 import type { Post } from '@/types/post';
 import { computed } from 'vue';
@@ -39,11 +40,46 @@ interface GalleryCardProps {
 
 const { data } = defineProps<GalleryCardProps>();
 
-const metaCaptions = computed(() => data.meta.map((meta) => ({
-    name: meta.name,
-    value: meta.value,
-    icon: mapMetaCodeToIcon[meta.name],
-})))
+const metaCaptions = computed(() => {
+  const captions = data.meta.map((meta) => {
+  if(meta.name === 'date'){
+    const date = new Date(meta.value);
+
+    const day = date.getDate();
+    const month = monthNames[date.getMonth()];
+
+    return ({
+      name: meta.name,
+      value: `${day} ${month}`,
+      icon: mapMetaCodeToIcon[meta.name],
+  })
+  }
+
+    return ({
+      name: meta.name,
+      value: meta.value,
+      icon: mapMetaCodeToIcon[meta.name],
+  })
+})
+
+function pluralizeComments(count: number) {
+  const mod10 = count % 10;
+  const mod100 = count % 100;
+
+  if (mod100 >= 11 && mod100 <= 14) return `${count} комментариев`;
+  if (mod10 === 1) return `${count} комментарий`;
+  if (mod10 >= 2 && mod10 <= 4) return `${count} комментария`;
+  return `${count} комментариев`;
+}
+
+  captions.push({
+    name: 'comments',
+    value: data.comments?.length ? pluralizeComments(data.comments?.length) : '0 комментариев',
+    icon: mapMetaCodeToIcon['comments'],
+  });
+
+  return captions;
+})
 
 </script>
 
