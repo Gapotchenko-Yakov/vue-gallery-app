6edfbdf20963efbad3fb7b7dc7c76708e2b0ca08
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-28 15:59:15 +0300

    test: remove max width constaint for vercel test

--- src/views/BlogPage.vue
+++ src/views/BlogPage.vue
@@ -14,8 +14,8 @@
     <div class="md:px-2.5 2xl:px-[30px] bg-gray-200 flex justify-center items-start gap-x-2.5">
       <div 
         v-if="filteredAndSearchedGalleryData.length > 0"
-        class="p-[15px] 2xl:p-[30px] bg-white rounded-container max-w-[1300px] w-full">
-        <div class="grid grid-cols-1 sm:grid-cols-2 2xl:grid-cols-3 gap-x-5 gap-y-[25px] 2xl:gap-y-10">
+        class="p-[15px] 2xl:p-[30px] bg-white rounded-container w-full">
+        <div class="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-x-5 gap-y-[25px] 2xl:gap-y-10">
           <gallery-card 
             v-for="card in filteredAndSearchedGalleryData"
             :key="card.id"
